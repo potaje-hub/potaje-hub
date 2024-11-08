2b1f433839c9fd7cffc83396dbcf0200d4db0c4b
Héctor Noguera <hecnoggon@alum.us.es>
2024-11-08 19:44:37 +0100

    fix:fakenodo

--- app/modules/fakenodo/routes.py
+++ app/modules/fakenodo/routes.py
@@ -1,3 +1,4 @@
+import uuid
 from flask import jsonify, make_response
 from app.modules.fakenodo import fakenodo_bp
 import json
@@ -12,7 +13,7 @@ def get_all():
 
 @fakenodo_bp.route('/fakenodo/deposit/depositions', methods=['POST'])
 def create():
-    response = make_response(jsonify({"message": "Deposition created", "id": 1}))
+    response = make_response(jsonify({"message": "Deposition created", "id": 1, "conceptrecid": 1}))
     response.status_code = 201
     return response
 
@@ -24,7 +25,24 @@ def upload(id):
     return response
 
 
+@fakenodo_bp.route('/fakenodo/deposit/depositions/<int:id>/actions/publish', methods=['POST'])
+def publish(id):
+    response = make_response(jsonify({"message": f"File uploaded to deposition {id}"}))
+    response.status_code = 202
+    return response
+
+
 @fakenodo_bp.route('/fakenodo/deposit/depositions/<int:id>', methods=['DELETE'])
 def delete(id):
     return jsonify({"message": f"Deposition {id} deleted"})
 
+
+@fakenodo_bp.route('/fakenodo/deposit/depositions/<int:id>', methods=['GET'])
+def get_deposition(id):
+    with open('app/modules/fakenodo/deposition.json') as f:
+        data = json.load(f)
+        # randomize doi with uuid
+
+        data['doi'] = str(uuid.uuid4())
+
+    return jsonify(data)
