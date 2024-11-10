079169620a146869e0080149fedc56247797f36e
Pablo Jesús Castellanos Compaña <pabcascom@alum.us.es>
2024-11-10 21:15:33 +0100

    feat: download all datasets

--- app/modules/dataset/repositories.py
+++ app/modules/dataset/repositories.py
@@ -113,6 +113,9 @@ class DataSetRepository(BaseRepository):
             .limit(5)
             .all()
         )
+        
+    def get_all_datasets(self):
+        return self.model.query.all()
 
 
 class DOIMappingRepository(BaseRepository):
