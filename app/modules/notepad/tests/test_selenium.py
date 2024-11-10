ca7561a9fbbde3134f38eeba476898505836cdbe
MiguelEncina <migencmar@alum.us.es>
2024-11-10 16:51:24 +0100

    prueba

--- app/modules/notepad/tests/test_selenium.py
+++ app/modules/notepad/tests/test_selenium.py
@@ -4,15 +4,17 @@ import time
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from core.selenium.common import initialize_driver
 
 class TestCreatenotepad():
   def setup_method(self, method):
-    self.driver = webdriver.Chrome()
+    self.driver = initialize_driver()
     self.vars = {}
   
   def teardown_method(self, method):
