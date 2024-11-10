ea75cc961c19aafe18b25b78888604ef341fddb7
MiguelEncina <migencmar@alum.us.es>
2024-11-10 16:54:17 +0100

    fix: Revert "minnor changes"
    This reverts commit ca7561a9fbbde3134f38eeba476898505836cdbe.

--- app/modules/notepad/tests/test_selenium.py
+++ app/modules/notepad/tests/test_selenium.py
@@ -4,17 +4,15 @@ import time
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from core.selenium.common import initialize_driver
 
 class TestCreatenotepad():
   def setup_method(self, method):
-    self.driver = initialize_driver()
+    self.driver = webdriver.Chrome()
     self.vars = {}
   
   def teardown_method(self, method):
