4390043113ea262d5c9ee3508af42adc0b76d41d
Héctor Noguera <hecnoggon@alum.us.es>
2024-12-02 19:54:34 +0100

    Feat: Add Download Splot Format test

--- app/modules/dataset/tests/test_selenium.py
+++ app/modules/dataset/tests/test_selenium.py
@@ -229,8 +229,58 @@ def test_download_dataset_DIMACS():
         close_driver(driver)        
 
 
+def test_download_dataset_splot():
+    driver = initialize_driver()
+    
+    try:
+        host = get_host_for_selenium_testing()
+
+        # Open the login page
+        driver.get(f"{host}/login")
+        wait_for_page_to_load(driver)
+
+        # Find the username and password field and enter the values
+        email_field = driver.find_element(By.NAME, "email")
+        password_field = driver.find_element(By.NAME, "password")
+
+        email_field.send_keys("user1@example.com")
+        password_field.send_keys("1234")
+
+        # Send the form
+        password_field.send_keys(Keys.RETURN)
+        wait_for_page_to_load(driver)
+
+        # Open the dataset list page
+        driver.get(f"{host}/dataset/list")
+        wait_for_page_to_load(driver)
+            
+        # Click on the first dataset in the list
+        first_dataset = driver.find_element(By.XPATH, "//table//tbody//tr[1]//td[1]//a")
+        first_dataset.click()
+        wait_for_page_to_load(driver)
+
+        # Click the dropdown to reveal the download all button
+            
+        dropdown = driver.find_element(By.ID, "btnGroupDonwloadAll")
+        driver.execute_script("arguments[0].click();", dropdown)
+        
+        wait_for_page_to_load(driver)
+
+        download_all_button = driver.find_element(By.ID, "download-splot")
+        driver.execute_script("arguments[0].click();", download_all_button)
+        wait_for_page_to_load(driver)
+        time.sleep(2)  # Force wait time
+        
+        print("Download all datasets test passed!")
+
+    finally:
+        # Close the browser
+        close_driver(driver)
+
+
 # Call the test function
-test_upload_dataset()
-test_download_dataset_glencoe()
-test_download_dataset_DIMACS()
+#test_upload_dataset()
+#test_download_dataset_glencoe()
+#test_download_dataset_DIMACS()
+test_download_dataset_splot()
 
