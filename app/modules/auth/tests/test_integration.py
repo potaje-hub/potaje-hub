7bd5162a412d5a3a04976df55b1b9102db4214e8
David Godoy Fernández <davgodfer.alumn.us.es>
2024-12-09 18:29:33 +0100

    test: login success test implemented

--- app/modules/auth/tests/test_integration.py
+++ app/modules/auth/tests/test_integration.py
@@ -12,4 +12,14 @@ def test_client(test_client):
         # Verify user
         token = AuthenticationService().get_token_from_email("test@example.com")
         AuthenticationService().confirm_user_with_token(token)
+
     yield test_client
+
+def test_login_success(test_client):
+    response = test_client.post(
+        "/login", data=dict(email="test@example.com", password="test1234"), follow_redirects=True
+    )
+
+    assert response.request.path != url_for("auth.login"), "Login was unsuccessful"
+
+    test_client.get("/logout", follow_redirects=True)
