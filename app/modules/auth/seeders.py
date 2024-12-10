dd66398a86b4f805a70abb56de9817d7aa1ecd3d
Fernando Jose de Celis Hurtado <ferdehur@alum.us.es>
2024-12-10 18:18:33 +0100

    feat: Github user added in seeders for user3

--- app/modules/auth/seeders.py
+++ app/modules/auth/seeders.py
@@ -13,7 +13,7 @@ class AuthSeeder(BaseSeeder):
         users = [
             User(email='user1@example.com', password='1234'),
             User(email='user2@example.com', password='1234'),
-            User(email='user3@example.com', password='1234', developer=True),
+            User(email='user3@example.com', password='1234', developer=True, github_user="GitUser3"),
         ]
 
         # Inserted users with their assigned IDs are returned by `self.seed`.
