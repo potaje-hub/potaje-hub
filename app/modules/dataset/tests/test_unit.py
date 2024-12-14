9be485ff04fbbf7667077a3b2b01e8009016e9d3
Pablo Jesús Castellanos Compaña <pabcascom@alum.us.es>
2024-12-14 15:13:55 +0100

    fix: test_client fixxed due to a confussion

--- app/modules/dataset/tests/test_unit.py
+++ app/modules/dataset/tests/test_unit.py
@@ -1,18 +1,16 @@
 import pytest
-from app import create_app
 
 
 @pytest.fixture(scope="module")
-def test_client():
-    flask_app = create_app()
-    testing_client = flask_app.test_client()
-
-    ctx = flask_app.app_context()
-    ctx.push()
-
-    yield testing_client
-
-    ctx.pop()
+def test_client(test_client):
+    """
+    Extends the test_client fixture to add additional specific data for module testing.
+    """
+    with test_client.application.app_context():
+        # Add HERE new elements to the database that you want to exist in the test context.
+        # DO NOT FORGET to use db.session.add(<element>) and db.session.commit() to save the data.
+        pass
+    yield test_client
 
 
 def test_download_all_dataset_not_logged_in(test_client):
