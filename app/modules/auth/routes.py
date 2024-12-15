673cedcf7e6035f4dcb60e3347169f921c578baa
David Godoy Fernández <davgodfer.alumn.us.es>
2024-12-15 21:08:23 +0100

    fix: fixed email sender password

--- app/modules/auth/routes.py
+++ app/modules/auth/routes.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'uvlhubio@gmail.com'
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
